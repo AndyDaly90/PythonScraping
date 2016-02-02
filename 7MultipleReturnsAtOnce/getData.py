61cbd4a2b90875b142e79ff1c4ce8a7a1bdd05c7
AndyDaly90 <andrew.daly@students.ittralee.ie>
2016-02-02 15:45:44 +0000

    7. Iterate through symbols and Save stock data to .txt

--- 7MultipleReturnsAtOnce/getData.py
+++ 7MultipleReturnsAtOnce/getData.py
@@ -7,18 +7,19 @@ symbolsList = open("symbols.txt").read()
 symbolsList = symbolsList.split("\n")
 
 for symbol in symbolsList:                      # + means create file
-    newFile = open("Prices/" + symbol + ".txt", "w+")
-    newFile.close()
+        newFile = open("Prices/" + symbol + ".txt", "w+")
+        newFile.close()
 
-    htmlText = urllib.urlopen("http://www.bloomberg.com/markets/chart/data/1D/" + symbol + ":US")
-    data = json.load(htmlText)
-    dataPoints = data["data_values"]
-    lastValue = dataPoints[-1][1]
+        htmlText = urllib.urlopen("http://www.bloomberg.com/markets/chart/data/1D/" + symbol + ":US")
+        data = json.load(htmlText)
+        dataPoints = data["data_values"]
+        lastValue = dataPoints[-1][1]
                                                 # append
-    newFile = open("Prices/" + symbol + ".txt", "a")
+        newFile = open("Prices/" + symbol + ".txt", "a")
 
-    print("Last Value for", symbol, lastValue)
-    time.sleep(2)
-    for point in dataPoints:
+        print("Last Value for", symbol, lastValue)
+        #time.sleep(2)
+        for point in dataPoints:
             newFile.write(str("Symbol: "+symbol+","+"Price: " + str(point[1]) + "\n"))
-    newFile.close()
+        newFile.close()
+print("Done")
